196e5c4ff8d4a7a654d985d14e8ccd806f74e24f
pc <devu>
2026-03-12 19:26:31 +0530

    backend portfolioss

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,9 +1,13 @@
 import "dotenv/config";
 // import { PrismaClient } from '../generated/prisma/client'
-import  {PrismaClient}  from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
+const globalForPrisma = globalThis
 
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient()
 
-const prisma = new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
-export default prisma  
+export default prisma
